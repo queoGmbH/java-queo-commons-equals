0f1e0e24aa3b58615f13fba3a0f58ebaf50abb8d
Ralph Engelmann <r.engelmann@queo-group.com>
2009-11-16 14:48:58 +0000

    Initial Import

---
+++ src/main/java/com/queomedia/commons/equals/EqualsChecker.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.queomedia.commons.equals;
+
+/**
+ * Compare objects by an different equals functionalty then there own.
+ * 
+ * This interface makes it possible to compare for example apples and oranges by equal weight.
+ * 
+ * @param <T> type of the first objects
+ * @param <K> type of the second objects
+ *
+ *
+ */
+public interface EqualsChecker<T, K> {
+
+    /**
+     * Calculate equality by specific attributes.
+     * @param objectT
+     * @param objectK
+     * @return true if the objects are equals by some specific attributes
+     */
+    boolean equals(T objectT, K objectK);
+}
